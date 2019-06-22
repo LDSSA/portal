c4580830f0c7df7ce5d1b7c105250b5d072440c5
Hugo Castilho <hcastilho@lisbondatascience.org>
2019-06-22 11:11:53 +0100

    Remove whitelist requirement

--- portal/users/adapters.py
+++ portal/users/adapters.py
@@ -25,6 +25,12 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
+
+class SocialAccountWhitelistAdapter(DefaultSocialAccountAdapter):
+
+    def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
+        return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
     def pre_social_login(self, request, sociallogin):
         try:
             UserWhitelist.objects.get(username=sociallogin.user)
