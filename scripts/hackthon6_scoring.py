4d5f82be7fdd51b7a3b9234172f4267233a283dc
Catarina Silva <catarina.cruz.csilva@gmail.com>
2021-01-09 19:09:32 +0000

    [HCKT06] Change scoring script for hackathon 6

--- scripts/hackthon6_scoring.py
+++ scripts/hackthon6_scoring.py
@@ -4,10 +4,37 @@ from sklearn import metrics
 
 from portal.capstone.models import DueDatapoint
 
+
+def fairness_score_precision(sensitive_class):
+    '''
+    Computes a fairness score corresponding to the highest difference
+     between precision among sensitive classes
+
+    :param sensitive_class: dictionary with the following structure
+      {
+       'sensitive_class_1': {'outcomes': [...], 'predictions': [...]},
+       'sensitive_class_2': {'outcomes': [...], 'predictions': [...]},
+       ...
+      }
+    :return: maximum difference between precisions in sensitive clasess
+    '''
+    precisions = []
+    for s_class in sensitive_class.keys():
+        outcomes = sensitive_class[s_class]["outcomes"]
+        predictions = sensitive_class[s_class]["predictions"]
+        if len(outcomes) > 100:
+            precisions.append(metrics.precision(outcomes, predictions))
+
+    precision_diff = 0
+    if precisions:
+        precision_diff = max(precisions) - min(precisions)
+
+    return precision_diff
+
+
 def score(student_api):
     '''
     Calculates the score of the students' API model
-
     :param student_api: StudentApi object
     :return: score as a float
     '''
@@ -15,7 +42,7 @@ def score(student_api):
     # Check which simulators have datapoints with outcomes outcomes
     simulator_ids = []
     for simulator in student_api.capstone.simulators.all():
-        if simulator.datapoints.filter(outcome__isnull=False).count() > 0:
+        if simulator.datapoints.exclude(outcome="").count() > 0:
             simulator_ids.append(simulator.id)
 
     if len(simulator_ids) == 0:
@@ -27,21 +54,46 @@ def score(student_api):
     )
     outcomes = []
     predictions = []
+    sensitive_class_race = {}
+    sensitive_class_sex = {}
     for ddp in qs:  # loop through each entry in DueDataPoint
-        if ddp.response_status != 200:  # Missing or bad response 
-            predictions.append(1 - ddp.datapoint.outcome)  # TODO what to do??
-            outcomes.append(ddp.datapoint.outcome)
+        outcome = json.loads(ddp.datapoint.outcome)
+        data = json.loads(ddp.datapoint.data)
+        if ddp.response_status != 200:  # Missing or bad response
+            predictions.append(not outcome)
+            outcomes.append(outcome)
 
         else:
             try:
                 prediction = json.loads(ddp.response_content)['prediction']
             except (json.JSONDecodeError, KeyError):
-                predictions.append(1 - ddp.datapoint.outcome)  # TODO what to do??
-                outcomes.append(ddp.datapoint.outcome)
-
+                predictions.append(not outcome)
+                outcomes.append(outcome)
             else:
-                predictions.append(prediction)
-                outcomes.append(ddp.datapoint.outcome)
+                sex = data["sex"].lower()
+                if sex not in sensitive_class_sex:
+                    sensitive_class_sex[sex] = {"outcomes": [], "predictions": []}
 
-    # Score
-    return metrics.roc_auc_score(outcomes, predictions)
+                sensitive_class_sex[sex]["outcomes"].append(outcome)
+                sensitive_class_sex[sex]["predictions"].append(prediction)
+
+                race = data["race"].lower()
+                if race not in sensitive_class_race:
+                    sensitive_class_race[race] = {"outcomes": [], "predictions": []}
+
+                sensitive_class_race[race]["outcomes"].append(outcome)
+                sensitive_class_race[race]["predictions"].append(prediction)
+
+                predictions.append(prediction)
+                outcomes.append(outcome)
+
+    f1_score = metrics.f1_score(outcomes, predictions, pos_label=True)
+
+    race_diff = fairness_score_precision(sensitive_class_race)
+    sex_diff = fairness_score_precision(sensitive_class_sex)
+
+    is_fair = race_diff < 0.2 and sex_diff < 0.2
+    if not is_fair:
+        f1_score = 0.0
+
+    return f1_score
