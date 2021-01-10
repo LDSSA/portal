6746e56e846a8b21ab8fbc69a9c452d0075ca04b
Hugo Castilho <hcastilho@lisbondatascience.org>
2021-01-10 14:35:36 +0000

    Increase

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -16,7 +16,7 @@ if READ_DOT_ENV_FILE:
 
 # CAPSTONE
 # ------------------------------------------------------------------------------
-TIMEOUT = 5
+TIMEOUT = 1
 SIMULATOR_INTERVAL = 5
 PRODUCER_INTERVAL = 1
 REQUEST_TIMEOUT = 10
