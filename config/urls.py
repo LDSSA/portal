f5df6f58663d259112fc11d5217142e78f751bba
Hugo Castilho <hcastilho@lisbondatascience.org>
2021-04-14 21:03:14 +0100

    Account management emails and setting password

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from allauth.account import views
 from portal.academy.views import HomeRedirectView
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     # path("",
     #      TemplateView.as_view(template_name="pages/home.html"),
     #      name="home"),
@@ -31,30 +32,6 @@ urlpatterns = [
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
-
-# User management
-urlpatterns += [
-    path("signup/", views.signup, name="account_signup"),
-    path("login/", views.login, name="account_login"),
-    path("logout/", views.logout, name="account_logout"),
-    # password reset
-    path("password/reset/",
-            views.password_reset,
-            name="account_reset_password"),
-    path("password/reset/done/",
-            views.password_reset_done,
-            name="account_reset_password_done"),
-    re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
-            views.password_reset_from_key,
-            name="account_reset_password_from_key"),
-    path("password/reset/key/done/",
-            views.password_reset_from_key_done,
-            name="account_reset_password_from_key_done"),
-]
-
-urlpatterns += [
-    path(r"accounts/email/", views.email, name="account_email"),
-]
 
 
 if settings.DEBUG:
