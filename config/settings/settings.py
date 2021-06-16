d6e6e499ea8022588b4ef9193c48eb6cf6b603f3
Hugo Castilho <hcastilho@lisbondatascience.org>
2021-06-16 16:31:13 +0100

    Add pii to sentry

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -570,7 +570,14 @@ if SENTRY_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
 
-    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
+    sentry_sdk.init(
+        dsn=SENTRY_DSN, 
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=.25,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        )
 
 # LOGGING
 # ------------------------------------------------------------------------------
