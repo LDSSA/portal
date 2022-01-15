fef30a60b7789d1e7f6a2f68baf76f467293ce9c
Hugo Castilho <hcastilho@lisbondatascience.org>
2022-01-15 00:23:19 +0000

    Fix scoring script

--- portal/capstone/management/commands/score-students.py
+++ portal/capstone/management/commands/score-students.py
@@ -1,20 +1,6 @@
-import sys
-import json
-import argparse
-import pandas as pd
-from sklearn.metrics import roc_auc_score
-from playhouse.shortcuts import model_to_dict
-
-# import settings
-import models
-
-import json
-
-import pandas as pd
 from django.core.management.base import BaseCommand
 
 from portal.capstone import models
-from portal.users.models import User
 
 
 class Command(BaseCommand):
