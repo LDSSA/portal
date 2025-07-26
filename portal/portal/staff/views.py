f73f3d1d3a41096557e26c0ffe241a4f641d0eee
majkah0 <mariahanulova@yahoo.com>
2025-07-26 13:40:59 +0200

    Repair academy start date

--- portal/portal/staff/views.py
+++ portal/portal/staff/views.py
@@ -109,7 +109,7 @@ class HomeView(AdmissionsStaffViewMixin, TemplateView):
             #    return HttpResponseServerError(
             #        b"error setting opening date. opening date must be before closing date",
             #    )
-
+            config.ACADEMY_START = datetime.now(timezone.utc)
             config.ADMISSIONS_APPLICATIONS_START = opening_date
 
         elif key == "applications_closing_date":
